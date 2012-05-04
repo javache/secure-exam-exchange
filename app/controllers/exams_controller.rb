5138629e4b3f39d3f7e0d4513894c0b4ae3771e7
Toon Willems <willemstoon@gmail.com>
2012-05-04 14:01:10 +0200

    add basic html views for creating exams, logic not yet implemented

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -1,4 +1,24 @@
 class ExamsController < ApplicationController
 
+  respond_to :html
+
+  def index
+    # TODO: show exams for the logged in user (which he can fill in)
+  end
+
+  def show
+    @exam = Exam.find(params[:id])
+  end
+
+  def create
+    exam = Exam.create!(params[:exam])
+    respond_with exam
+  end
+
+  def new
+    @exam = Exam.new
+    @users = User.all
+  end
+
 
 end
